786c1290587528fa3092832af7ac3c79ab6809f5
Mudassir Malik <mudassir.ainex@gmail.com>
2025-08-06 06:27:09 +0000

    Vertical scroll is still there, please reduce the height of chat thread

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -19,8 +19,12 @@ export default async function ChatPage({ searchParams }: ChatPageProps) {
     
     const chats = await getChats(currentUser.uid);
 
+    // This height calculation ensures the chat layout fills the vertical space
+    // without causing the main page to scroll. It accounts for the header height and main padding.
+    // lg:h-[calc(100vh_-_8rem)] is for large screens (main padding p-8 = 2rem * 2 = 4rem, header h-16 = 4rem)
+    // h-[calc(100vh_-_7rem)] is for smaller screens (main padding p-4/p-6, header h-16)
     return (
-      <div className="h-full">
+      <div className="h-[calc(100vh_-_7rem)] lg:h-[calc(100vh_-_8rem)]">
         <ChatLayout 
             chats={chats} 
             defaultChatId={searchParams?.chatId} 
