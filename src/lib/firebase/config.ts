849bbf747a4419c2a8f1b299b7c554a4ea0a706a
Mudassir Malik <mudassir.ainex@gmail.com>
2025-08-06 16:21:15 +0000

    revert storage related changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   projectId: "clipperconnect-91021",
   appId: "1:1071521772936:web:3044d8320225060ea6363d",
-  storageBucket: "clipper-connect",
+  storageBucket: "clipperconnect-91021.appspot.com",
   apiKey: "AIzaSyBhwfzTewUm-D1tkKV1In7PwpNQDHVMYxg",
   authDomain: "clipperconnect-91021.firebaseapp.com",
   measurementId: "",
