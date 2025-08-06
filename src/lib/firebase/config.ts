c911c7644ba7e055e06133ec05e5ac19bf089d2d
Mudassir Malik <mudassir.ainex@gmail.com>
2025-08-06 16:04:37 +0000

    I have a bucket in google cloud console, is there a way you can connect

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   projectId: "clipperconnect-91021",
   appId: "1:1071521772936:web:3044d8320225060ea6363d",
-  storageBucket: "clipperconnect-91021.appspot.com",
+  storageBucket: "clipper-connect",
   apiKey: "AIzaSyBhwfzTewUm-D1tkKV1In7PwpNQDHVMYxg",
   authDomain: "clipperconnect-91021.firebaseapp.com",
   measurementId: "",
